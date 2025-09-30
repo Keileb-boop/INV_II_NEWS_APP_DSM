f284daacf753835ac1611ab752c0fcb0368d5501
Keileb-boop <125236416+Keileb-boop@users.noreply.github.com>
2025-09-29 23:50:15 -0600

    Actualizando la NewsAdapater
    Aqui añadimos la función de que el titulo sea la url y al presionarla se envie al navegador

--- appnoticias2/app/src/main/java/sv/udb/api/ui/main/NewsAdapter.kt
+++ appnoticias2/app/src/main/java/sv/udb/api/ui/main/NewsAdapter.kt
@@ -1,12 +1,13 @@
 package sv.edu.api.ui.main
 
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import sv.edu.api.data.model.Article
 import sv.udb.api.databinding.ItemNewsBinding
-import sv.udb.api.databinding.ActivityMainBinding
 
 class NewsAdapter(private val articles: MutableList<Article>) :
     RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
@@ -23,12 +24,25 @@ class NewsAdapter(private val articles: MutableList<Article>) :
 
     override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
         val article = articles[position]
-        holder.binding.tvTitle.text = article.title
-        holder.binding.tvDescription.text = article.description
 
+        //Para mostrar el titulo de la noticia
+        holder.binding.tvTitle.text = article.title ?: "Sin título"
+
+      // Mostrar la descripción de la noticia
+        holder.binding.tvDescription.text = article.description ?: "Sin descripción"
+
+       //Cargar la imagen de la noticia
         Glide.with(holder.itemView.context)
             .load(article.urlToImage)
             .into(holder.binding.ivNews)
+
+        //Haciendo click en la noticia nos envia a la página web en el navegador del celular
+        holder.binding.tvTitle.setOnClickListener {
+            article.url?.let { link ->
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(link))
+                holder.itemView.context.startActivity(intent)
+            }
+        }
     }
 
     fun updateData(newArticles: List<Article>) {
