ca90771559f8cda4362fb89d1b0e8a54c41cec76
Keileb-boop <125236416+Keileb-boop@users.noreply.github.com>
2025-09-29 23:51:33 -0600

    Actualizando clase Article
    Para reconocer el titulo como URL de la noticia

--- appnoticias2/app/src/main/java/sv/udb/api/data/model/Article.kt
+++ appnoticias2/app/src/main/java/sv/udb/api/data/model/Article.kt
@@ -3,5 +3,6 @@ package sv.edu.api.data.model
 data class Article(
     val title: String?,
     val description: String?,
-    val urlToImage: String?
+    val urlToImage: String?,
+    val url: String?   // Aqui se agrega para que reconozca la NewsAdapter que el titulo sea la url
 )
